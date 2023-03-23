9409f1da6d5f36b922d73fd1f55071f505f7db9f
Fangpower <mikeydowsett@gmail.com>
2023-03-23 12:01:40 -0500

    More Array Practice

---
+++ repos/CandyArray/Program.cs
@@ -0,0 +1,26 @@
+﻿/**
+* @author MikeyDowsett
+* Class Program gives values to array bag before
+* dispaying the value of each item.
+*/
+internal class Program {
+    const int MAX_ITEMS = 3;
+
+    static void Main(string[] args) {
+        Candy[] bag = new Candy[MAX_ITEMS];
+        FillBag(bag);
+        ShowBagContents(bag);
+    }
+
+    static void FillBag(Candy[] bag) {
+        bag[0] = new Candy("KitKat", 1.0m);
+        bag[1] = new Candy("Milky Way", 0.9m);
+        bag[2] = new Candy("Jolly Rancher", 1.25m);
+    }
+
+    static void ShowBagContents(Candy[] bag) {
+        foreach (Candy candy in bag) {
+            Console.WriteLine(candy.ToString());
+        }
+    }
+}
