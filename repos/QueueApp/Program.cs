2304867a30c837a122c461390a75a2d0cd8d3148
Fangpower <mikeydowsett@gmail.com>
2023-05-10 14:02:08 -0500

    Update Program.cs

--- repos/QueueApp/Program.cs
+++ repos/QueueApp/Program.cs
@@ -11,6 +11,16 @@ class Program {
 
         //Create Ticket Windows
         TicketWindows ticketWindows = new TicketWindows();
+
+        //Open the windows to the customers.
+        TicketWindows windows = new TicketWindows();
+        Thread window1 = new Thread(windows.WindowOne);
+        Thread window2 = new Thread(windows.WindowTwo);
+        Thread window3 = new Thread(windows.WindowThree);
+        window1.Start(lines[0]);
+        window2.Start(lines[1]);
+        window3.Start(lines[2]);
+        Console.WriteLine("Main Finished");
     }
 
     public static void ReadFile(string filename, Queue<Person>[] lines) {
@@ -27,9 +37,11 @@ class Program {
         int qNumber = 0;
         while ((record = inFile.ReadLine()) != null) {
             string[] data = record.Split(',');
-            lines[qNumber].Enqueue(new Person(data[0], data[1], int.Parse(data[2])));
+            lines[qNumber].Enqueue(new Person(data[0], data[1],
+                int.Parse(data[2])));
             qNumber = (qNumber + 1) % lines.Length;
         }
         inFile.Close();
     }
+
 }
